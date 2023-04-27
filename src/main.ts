446f7e956860d6597c06ded02ea02df014432272
Andrei Gorokhov <a.gorokhov@replika.ai>
2023-04-27 09:58:35 +0300

    prod dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,26 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const githubToken = core.getInput('github_token', {required: true})
+    const sourceBranch = github.context.ref.replace(/^refs\/heads\//, '')
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const credentials = {
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo
+    }
 
-    core.setOutput('time', new Date().toTimeString())
+    const octokit = github.getOctokit(githubToken)
+
+    const branchHead = `${credentials.owner}:${sourceBranch}`
+    const {data} = await octokit.rest.pulls.list({
+      ...credentials,
+      base: 'main',
+      head: branchHead
+    })
+
+    core.info(data.toString())
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
